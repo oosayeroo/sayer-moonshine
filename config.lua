882042054e9d716ff9ed9a3739f38b77748eeaa9
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2023-01-26 00:40:45 +0000

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,7 +1,51 @@
 Config = {}
 
-Config.JimMenu = false --set true if using jims menu found here https://github.com/jimathy/qb-menu
+Config.JimShops = false --set true if using jims menu found here https://github.com/jimathy/qb-menu
+Config.MoneyType = 'cash' --what account you wanna use / cash/bank/blackmoney etc
+Config.Items = { --these are items you require so if you want to change them to an existing item in your server change the second variable so ["Trowel"] = changeme,
+--DO NOT CHANGE THE FIRST VARIABLE THAT IS NEEDED!!!
+    ["Trowel"] = "trowel",
+    ["Waterjar"] = "m-waterjar",
+    ["Yeast"] = "m-yeast",
+    ["Potato"] = "m-potato",
+    ["Barley"] = "m-barley",
+    ["Mash"] = "m-mash",
+    ["HeatedWater"] = "m-heatedwater",
+    ["HeatedBarley"] = "m-heatedbarley",
+    ["HeatedMixture"] = "m-heatedmixture",
+    ["MoonshineMix"] = "m-moonshinemix",
+    ["Moonshine"] = "m-moonshine"
+}
 
+Config.ShopBlip = true
+Config.Shop = {
+    ["Sprite"] = 52,
+    ["Colour"] = 1,
+    ["Size"] = 0.5,
+    ["Label"] = "Moonshine Ingredients",
+}
+Config.IngredientShop = {
+    label = "Moonshine Ingredients",
+        slots = 6,  --make sure this number is the same as the amount of items you have in this list or it wont work
+        items = {
+            [1] = {
+                name = "m-waterjar",
+                price = 5,
+                amount = 50,
+                info = {},
+                type = "item",
+                slot = 1,
+            },
+            [2] = {
+                name = "m-yeast",
+                price = 5,
+                amount = 50,
+                info = {},
+                type = "item",
+                slot = 2,
+            },
+        }
+}
 Config.EnableLocations = "enabled"  --dont change this as third eye not setup
 Config.MoonshineLocations = {
     ["pick-potato1"] = {    --picking locations have a 20 metre radius. so best not to put them too close together. i designed it to work in a field
@@ -24,7 +68,7 @@ Config.MoonshineLocations = {
     ["add-yeast"] = {  --stage 3
         [1] = vector3(1394.57, 3601.86, 38.94),
     },
-    ["add-mash"] = {   --stage 4
+    ["ferment-mash"] = {   --stage 4
         [1] = vector3(1396.66, 3605.12, 38.94),
     },
     ["finish-moonshine"] = {   --stage 5
@@ -41,17 +85,11 @@ Config.PickingRadius = 20 --how big the area you can pick at picking locations (
 Config.PotatoHarvestAmount = math.random(1, 4)  --amount of potato to harvest each time
 Config.BarleyHarvestAmount = math.random(1, 4)  -- amont of barley to harvest each time
 
---amounts needed--
+--amounts --
 Config.MashAmountNeeded = 1 --amount of mash you need to add to make next step
 Config.YeastAmountNeeded = 1
 Config.BarleyAmountNeeded = 1
-
---shop--
-Config.IngredientCost = math.random(100, 150)    --how much it costs for the jars
-Config.JarAmount = math.random(10, 20)  --how many jars you purchase for price above
-Config.YeastAmount = math.random(10, 20)  --amount of yeast to buy in one purchase
 Config.MashAmount = math.random(1, 3)  --amount of mash to get from 1 potato
-Config.YeastCost = math.random(20, 50)  -- cost of purchasing yeast
 Config.BatchAmount = math.random(3, 8)     --how many moonshine you end up with by end of all stages
 
 --selling--
@@ -61,6 +99,6 @@ Config.MoonshineValue = 100  --how much 1 moonshine sells for at sell location
 --BREAK/FAIL SYSTEM--
 Config.EnableFailing = true --can fail a process
 Config.LoseOnFail = false --makes it so you lose the item upon failing if you have enablefailing = true
-Config.FailChance = 10 --in % chance that you fail a step.
-
-
+Config.FireOnFail = true --[VERY FUN] [BOOM]
+Config.FailChance = 20 --in % chance that you fail a step.
+Config.FireChance = 5 --in % out of 100 
